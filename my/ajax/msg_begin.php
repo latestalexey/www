c7572d51b900a49d3e21fe300552285c2b2304dd
ovirab <info@virab.ru>
2016-07-28 00:00:44 +0300

    virab_2807

--- my/ajax/msg_begin.php
+++ my/ajax/msg_begin.php
@@ -1,13 +1,15 @@
-<div id="msg_begin" style="background-color: #FFF; padding: 35px 0px;">
-	<div class="msg_begin_avatar" style="background-image: <?=$_POST["contact_img"]?>;">
-	</div>
-	<div class="message_header" style="vertical-align: 30px; font-size: 20px;">
-		<?
-		$contactName = ($_POST["fullname"] == "" || $_POST["fullname"] == $_POST["contact"])?($_POST["contact"]):($_POST["fullname"]." '".$_POST["contact"]."'");
-		?>
-		<a><?=$contactName?></a>
-	</div>
-	<div style="font-weight: 600; padding: 5px 20px; font-size: 17px; color: #607d8b;">
-		Здесь самое начало вашего общения с <?=$contactName?> в TELEPORT
+<div data-ms-inf="<?=$_POST["contact"]?>">
+	<div id="msg_begin" style="background-color: rgba(136,198,156,0.4); padding: 35px 0px;">
+		<div class="msg_begin_avatar" style="background-color: #FFF; background-image: <?=($_POST["contact_img"]=='none')?('url(/include/no_avatar.svg)'):($_POST["contact_img"])?>;">
+		</div>
+		<div class="message_header" style="vertical-align: 30px; font-size: 20px; color: #FF6C00">
+			<?
+			$contactName = ($_POST["fullname"] == "" || $_POST["fullname"] == $_POST["contact"])?($_POST["contact"]):($_POST["fullname"]." '".$_POST["contact"]."'");
+			?>
+			<a><?=$contactName?></a>
+		</div>
+		<div style="font-weight: 600; padding: 5px 20px; font-size: 17px; color: #FF6C00;">
+			Здесь самое начало вашего общения с <?=$contactName?> в TELEPORT
+		</div>
 	</div>
 </div>
