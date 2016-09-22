b46192c290a10a622b34f90b6bd12525671f047e
ovirab <info@virab.ru>
2016-09-22 17:08:19 +0300

    open

--- auth/index.php
+++ auth/index.php
@@ -24,42 +24,7 @@ else {
 }	
 ?>	
 <div id="back-window"> <!--min-height: 650px; -->
-	<div id="big_logo"></div>
-	<div style="margin: 20px 0 0 0; text-align: center;">
-		<img src="/include/teleport_header.png">
-		<div style="font-size: 24px; margin-top: 20px; text-align: center; color: #0e9487; font-family: 'Open sans';">Clients Environment Optimization</div>
-		<hr style="width: 950px; border: 1px solid #ff6600;">
-	</div>
-	<div style="margin: 20px 0 0 0;">
-		<div style="font-size: 22px; margin-top: 7px; text-align: center; color: #FF6600; font-family: 'Open sans'; font-weight: 600;">Товары от производителей и импортеров по оптовым ценам</div>
-		<div style="margin: 25px auto; border: 2px solid #0e9487; height: 50px; border-radius: 5px; max-width: 750px; position: relative;">
-			<input style="width: 100%;height: 100%;font-size: 21px;padding: 0 5px 0 5px; font-family: 'Open sans';" id="all_search_inp" type="text" placeholder="Название товара, бренда или компании">
-			<div id="all_search_button">
-				<svg fill="#FF6600" height="45px" width="45px" version="1.1" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink">
-					<g fill="none" fill-rule="evenodd" id="Page-1" stroke="none" stroke-width="1"></g>
-					<path d="M19.4271164,21.4271164 C18.0372495,22.4174803 16.3366522,23 14.5,23 C9.80557939,23 6,19.1944206 6,14.5 C6,9.80557939 9.80557939,6 14.5,6 C19.1944206,6 23,9.80557939 23,14.5 C23,16.3366522 22.4174803,18.0372495 21.4271164,19.4271164 L27.0119176,25.0119176 C27.5621186,25.5621186 27.5575313,26.4424687 27.0117185,26.9882815 L26.9882815,27.0117185 C26.4438648,27.5561352 25.5576204,27.5576204 25.0119176,27.0119176 L19.4271164,21.4271164 L19.4271164,21.4271164 Z M14.5,21 C18.0898511,21 21,18.0898511 21,14.5 C21,10.9101489 18.0898511,8 14.5,8 C10.9101489,8 8,10.9101489 8,14.5 C8,18.0898511 10.9101489,21 14.5,21 L14.5,21 Z" id="search"/>
-				</svg>
-			</div>
-		</div>
-	</div>
-	<div style="margin: 70px 0 0 0; padding: 10px 0; background-color: #607D8B;">
-		<div style="margin: 20px auto 20px auto; width: 950px; background-color: #FFF;">
-			<img style="width: 100%;" src="http://isonex.ru/include/action_1.jpg">
-			<div style="padding: 15px; border-top: 1px solid #CCC; font-family: 'Open sans'; font-size: 23px; color: #777;">
-				<div class="block_menu">О компании</div>
-				<div class="block_menu">Новости</div>
-				<div class="block_menu">Каталог товаров</div>
-			</div>
-		</div>
-		<div style="margin: 20px auto 20px auto; width: 950px; background-color: #FFF;">
-			<img style="width: 100%;" src="http://static.price.ru/-/home/head_banner_sapogi.jpg">
-			<div style="padding: 15px; border-top: 1px solid #CCC; font-family: 'Open sans'; font-size: 23px; color: #777;">
-				<div class="block_menu">О компании</div>
-				<div class="block_menu">Новости</div>
-				<div class="block_menu">Каталог товаров</div>
-			</div>
-		</div>
-	</div>
+	<?include($_SERVER["DOCUMENT_ROOT"]."/open/index.php");?>
 </div>
 	<div id="login-window" class="modal_window" style="display: block;">
 	<div class="close_line" style="<?=$cl_style;?>"><div class="clw"><img src="/include/close_window.svg"/></div></div>
